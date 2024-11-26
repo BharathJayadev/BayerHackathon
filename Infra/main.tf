5d7188aa723733d952fea04973d9c045e9df52f0
Bharath <134907687+BharathJayadev@users.noreply.github.com>
2024-11-26 17:10:21 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 # VPC creation
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     Name = "main-vpc"
@@ -79,7 +79,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
   }
 
@@ -124,7 +124,4 @@ resource "aws_security_group" "ecs" {
 # ECS Cluster
 resource "aws_ecs_cluster" "main" {
   name = var.ecs_cluster_name
-}
-
-
 }
